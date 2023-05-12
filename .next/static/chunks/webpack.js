a638523c92f3ff5411f13196f3af620665bd1c10
Adam Black <fernando@notoriovs.com>
2023-05-12 11:12:15 -0600

    Remove .next dir

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b1a21057338de812"; }
+/******/ 		__webpack_require__.h = function() { return "2a63e6b2ce75208c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
