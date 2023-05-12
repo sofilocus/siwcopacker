d09035d486b83347001d124c5161ecd6e1b816e2
Adam Black <fernando@notoriovs.com>
2023-05-12 11:09:40 -0600

    Added <Head> to landing pages

--- src/pages/toallitas-humedas-individuales.js
+++ src/pages/toallitas-humedas-individuales.js
@@ -1,13 +1,22 @@
 import About from "../components/landing/about";
 import Hero from "../components/landing/hero";
+import Head from "next/head";
 
 export default function Landing() {
   return (
-    <section className="text-neutral-600 w-screen flex flex-col items-center justify-center">
-      <Hero />
-      <div className="md:max-w-7xl w-screen flex flex-col items-center justify-center container mx-auto">
-        <About />
-      </div>
-    </section>
+    <>
+      <Head>
+        <title>Toallitas Húmedas Individuales</title>
+        <meta name="description"
+              content="Expertos en personalizar toallitas individuales con el mensaje de tu marca. Convertimos un producto común en uno portátil, práctico y listo para usarse." />
+      </Head>
+      <h1 className="main_headline">Pequeños empaques, gran difusión</h1>
+      <section className="text-neutral-600 w-screen flex flex-col items-center justify-center">
+        <Hero />
+        <div className="md:max-w-7xl w-screen flex flex-col items-center justify-center container mx-auto">
+          <About />
+        </div>
+      </section>
+    </>
   );
 }
