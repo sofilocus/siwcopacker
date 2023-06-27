b5659772b8a22ac896f755f8cc6b93d840f72f1d
Adam Black <fernando@notoriovs.com>
2023-06-27 11:02:06 -0600

    Added modal for Toallitas IPA 70%

--- src/pages/toallitas-humedas-individuales.js
+++ src/pages/toallitas-humedas-individuales.js
@@ -1,10 +1,44 @@
 import About from "../components/landing/about";
 import Hero from "../components/landing/hero";
 import Head from "next/head";
+import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import s4 from "../../public/assets/08.png";
 
 export default function Landing() {
+  const [showModal, setShowModal] = useState(true);
+
+  const renderModal = () => {
+    return (
+      <div className="fixed flex justify-center items-center inset-0 bg-black/50 p-20 z-50">
+        <div className="relative flex w-full md:w-1/2 bg-white rounded-xl p-20">
+          <div className="flex flex-col md:w-1/2">
+            <h2>Toallita con alcohol IPA 70%</h2>
+            <p>Toallitas Humedas Individuales Impregnadas con alcohol Isopropílico
+              para la limpieza de pequeñas superficies.</p>
+            <Link href="#contact">
+              <a className="mt-8 bg-[#1da1f2] p-4 rounded-lg after:content-['›'] after:pl-4 text-white" onClick={() => setShowModal(false)}>Contáctanos</a>
+            </Link>
+          </div>
+          <div className="relative md:w-1/2">
+            <Image
+              src={s4}
+              layout="fill"
+              objectFit="contain"
+            />
+          </div>
+          <div className='cursor-pointer' onClick={() => setShowModal(false)}>
+            <span className="absolute top-4 right-4 ft-4 material-icons">close</span>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <>
+      {showModal && renderModal()}
       <Head>
         <title>Toallitas Húmedas Individuales</title>
         <meta name="description"
